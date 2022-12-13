5d2e9f71c1a9653d008f49067fb063ea6fd1619c
Bolec <Bolec@DESKTOP-KA0JV61>
2022-12-13 19:54:14 +0100

    Add project files.

---
+++ HelloWord/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloWord
+{
+    //PascalCase
+    public  class Program
+    { 
+        
+        public static void Main(string[] args)
+        {
+            //camelCase
+            string firstAndLAstName = "Błażej";
+
+            //UPPER_CASE
+            const double PI_NUMBER = 3.14;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Nazywam się Blazej Kociuba");
+            if (PI_NUMBER > 2)
+            {
+            }
+        }
+        //między metodami robimy maksymalnie jedną linijke przerwy
+        private string WritePINumber (double pi) //to jest metoda
+        {
+            return ("3,14");
+        }
+    }
+
+    
+}
+    
+
