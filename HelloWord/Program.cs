bc94e715595ddb93129fa4f03d835b8370211ab2
Bartek Kociuba <bar500@interia.pl>
2022-12-20 20:32:37 +0100

    Modified Program - added new WriteLine Addes TEst2 class - empty class

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -9,12 +9,13 @@ namespace HelloWord
         public static void Main(string[] args)
         {
             //camelCase
-            string firstAndLAstName = "Błażej";
+            //string firstAndLAstName = "Błażej";
 
             //UPPER_CASE
             const double PI_NUMBER = 3.14;
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Nazywam się Blazej Kociuba");
+            Console.WriteLine("nowa zmiana na potrzeby GitHub-a");
             if (PI_NUMBER > 2)
             {
             }
